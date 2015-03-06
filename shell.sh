1d8c7b7b15a5ceb16e900c4f4dea3485177c1de3
Amir TOLY <amir.toly@gmail.com>
2015-03-06 19:15:07 +0100

    Create a database

---
+++ shell.sh
@@ -0,0 +1,9 @@
+# Create a database named mydb
+'/Applications/Postgres.app/Contents/Versions/9.4/bin'/createdb mydb
+
+# Drop mydb database
+'/Applications/Postgres.app/Contents/Versions/9.4/bin'/dropdb mydb
+
+# Recreate it
+'/Applications/Postgres.app/Contents/Versions/9.4/bin'/createdb mydb
+
