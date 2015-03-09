506b31bd346e32ee9421fd1d4fe20742ba3b5795
Amir TOLY <amir.toly@gmail.com>
2015-03-09 18:36:25 +0100

    Create tables

--- sql.sql
+++ sql.sql
@@ -7,3 +7,31 @@ SELECT current_date;
 -- Add 2 and 2
 SELECT 2 + 2;
 
+-- Create WEATHER table
+CREATE TABLE weather (
+  city    varchar(80),
+  temp_lo int,          -- low temperature
+  temp_hi int,          -- high temperature
+  prcp    real,         -- precipitation
+  date    date
+);
+
+-- Create CITIES table
+CREATE TABLE cities (
+  name      varchar(80),
+  location  point
+);
+
+-- List tables (among other things) with \d
+
+-- Remove CITIES table
+DROP TABLE cities;
+
+-- List tables again
+
+-- Re-create CITIES table
+CREATE TABLE cities (
+  name      varchar(80),
+  location  point
+);
+
