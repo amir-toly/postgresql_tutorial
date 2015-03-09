94b4f904529f61017737f807c3e8b313e9568522
Amir TOLY <amir.toly@gmail.com>
2015-03-09 09:07:35 +0100

    Access a database

--- shell.sh
+++ shell.sh
@@ -7,3 +7,6 @@
 # Recreate it
 '/Applications/Postgres.app/Contents/Versions/9.4/bin'/createdb mydb
 
+# Run psql to access mydb database
+'/Applications/Postgres.app/Contents/Versions/9.4/bin'/psql mydb
+
---
+++ sql.sql
@@ -0,0 +1,9 @@
+-- Display PostgreSQL version
+SELECT version();
+
+-- Display current date
+SELECT current_date;
+
+-- Add 2 and 2
+SELECT 2 + 2;
+
