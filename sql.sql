89f8708540b0374bd04da6b38a4f45c1f598d293
Amir TOLY <amir.toly@gmail.com>
2015-04-08 08:55:32 +0200

    Create views

--- sql.sql
+++ sql.sql
@@ -184,3 +184,12 @@ VALUES ('San Francisco', 43, 57, 0.0, '1994-11-29');
 INSERT INTO weather (date, city, temp_hi, temp_lo)
 VALUES ('1994-11-29', 'Hayward', 54, 37);
 
+-- Define a view
+CREATE VIEW myview AS
+SELECT city, temp_lo, temp_hi, prcp, date, location
+FROM weather, cities
+WHERE city = name;
+
+-- Display the view
+SELECT * FROM myview;
+
